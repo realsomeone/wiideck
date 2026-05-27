c33913cb4091732652ae2c9364605208817aa934
realsomeone01 <arnaldohalicea@gmail.com>
2026-05-27 16:25:37 -0400

    fixed hard-coded path. working on un-hard-coding and MacOS

--- actions.py
+++ actions.py
@@ -1,21 +1,28 @@
-import subprocess
-import keyboard
 import os
 import pwd
+import subprocess
+
+import keyboard
 
 USER = os.environ.get("SUDO_USER", os.environ.get("USER"))
 UID = pwd.getpwnam(USER).pw_uid
 
-DIR = "/home/arnal/Documents/wiimote.py/"
+DIR = "/home/arnal/Documents/wiideck/"
+
 
 def sfx(file):
     print("sounding")
     subprocess.Popen(
-        ["su", USER, "-c", 
-         f"XDG_RUNTIME_DIR=/run/user/{UID} paplay --device=sfx '{DIR+file}'"],
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        [
+            "su",
+            USER,
+            "-c",
+            f"XDG_RUNTIME_DIR=/run/user/{UID} pw-play --target sfx --volume 0.2 '{DIR+file}'",
+        ],
     )
+
 
 def bind(keys):
     print("binding")
     keyboard.send(keys)
+
